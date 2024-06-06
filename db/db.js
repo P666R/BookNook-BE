8b313780084bbc2a035f78ff493788713c4ae972
PRoy <sam469roger@gmail.com>
2024-06-06 14:21:14 +0530

    updated router, new user router, db functionality

--- db/db.js
+++ db/db.js
@@ -1,7 +1,22 @@
 const mongoose = require('mongoose');
+const User = require('../models/userModel');
 
-exports.connect = async () => {
+const connect = async () => {
   await mongoose.connect(process.env.MONGO_URI);
-
   console.log('Database connected...');
 };
+
+const disconnect = async () => {
+  await mongoose.connection.close();
+  console.log('Database disconnected...');
+};
+
+const findUser = async (obj) => {
+  return await User.findOne(obj);
+};
+
+const saveUser = async (newUser) => {
+  return await newUser.save();
+};
+
+module.exports = { connect, disconnect, findUser, saveUser };
--- models/userModel.js
+++ models/userModel.js
@@ -1,77 +1,39 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
-const userSchema = new mongoose.Schema(
-  {
-    firstName: {
-      type: String,
-      required: [true, 'First name is required'],
-      trim: true,
-      minLength: [2, 'First name must be at least 2 characters long'],
-      maxLength: [32, 'First name cannot be longer than 32 characters'],
-      validate: {
-        validator: (value) => /^[a-zA-Z]+$/.test(value),
-        message: 'First name can only contain letters (a-zA-Z)',
-      },
-    },
-    lastName: {
-      type: String,
-      required: [true, 'Last name is required'],
-      trim: true,
-      minLength: [2, 'Last name must be at least 2 characters long'],
-      maxLength: [32, 'Last name cannot be longer than 32 characters'],
-      validate: {
-        validator: (value) => /^[a-zA-Z]+$/.test(value),
-        message: 'Last name can only contain letters (a-zA-Z)',
-      },
-    },
-    address: {
-      type: String,
-      required: [true, 'Address is required'],
-      trim: true,
-    },
-    city: {
-      type: String,
-      required: [true, 'City is required'],
-      trim: true,
-      validate: {
-        validator: (value) => /^[a-zA-Z\s]+$/.test(value),
-        message: 'City can only contain letters (a-zA-Z) and spaces',
-      },
-    },
-    state: {
-      type: String,
-      required: [true, 'State is required'],
-      trim: true,
-      validate: {
-        validator: (value) => /^[a-zA-Z\s]+$/.test(value),
-        message: 'State can only contain letters and spaces',
-      },
-    },
-    zipCode: {
-      type: String,
-      required: [true, 'Zip code is required'],
-      trim: true,
-    },
-    email: {
-      type: String,
-      lowercase: true,
-      required: [true, 'Email is required'],
-      validate: [validator.isEmail, 'Please enter a valid email'],
-    },
-    password: {
-      type: String,
-      required: [true, 'Password is required'],
-      select: false,
-      validate: [
-        validator.isStrongPassword,
-        'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number and one special character.',
-      ],
-    },
+const userSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  firstName: {
+    type: String,
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  lastName: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  zipCode: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
 module.exports = mongoose.model('User', userSchema);
